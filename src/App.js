adf343042a50c540ca35062d109413b694958a27
ross-u <cirkovic.uros.srb@gmail.com>
2020-02-24 13:04:57 +0100

    Update example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./App.css"; // with React CSS files are imported directly into the .js file
 
+import MyComponent from "./MyComponent";
+
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <h1> Hello Ironhackers! </h1>
         <h3> React Intro </h3>
+
+        <MyComponent />
+
+        <MyComponent city="Barcelona" />
       </div>
     );
   }
---
+++ src/MyComponent.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class MyComponent extends Component {
+  state = {
+    name: "Sarah",
+    city: "Paris"
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Hello {this.state.name} </h2>
+        <p>Welcome to {this.props.city} </p>
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
